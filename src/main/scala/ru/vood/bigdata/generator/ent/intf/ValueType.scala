79ea3df5cc79015c97edbafa0a4f9f0021187391
vood <balitsky_igor@list.ru>
2021-06-08 21:03:33 +0300

    init

--- src/main/scala/ru/vood/bigdata/generator/ent/intf/ValueType.scala
+++ src/main/scala/ru/vood/bigdata/generator/ent/intf/ValueType.scala
@@ -1,17 +1,38 @@
 package ru.vood.bigdata.generator.ent.intf
 
-sealed trait ValueType {
+import java.sql.Date
+import java.time.{LocalDate, LocalDateTime}
 
+sealed trait ValueType[R] {
+
+  implicit val defaultStr: AnyVal => String = { q => q.hashCode().toString }
+
+  implicit val defaultNum: AnyVal => Number = { q => q.hashCode() }
+
+  implicit val defaultDate: AnyVal => LocalDateTime = { q =>
+
+    new LocalDateTime(LocalDate.EPOCH).plusHours(q.hashCode())
+
+  }
+
+  def defaultGen(t: AnyVal)(implicit mutate: AnyVal => R): R
 
 }
 
 object ValueType {
 
-  case object Str extends ValueType
 
-  case object Num extends ValueType
+  case object Str extends ValueType[String] {
+    override def defaultGen(t: AnyVal)(implicit mutate: AnyVal => String): String = mutate(t)
+  }
 
-  case object Date extends ValueType
+  case object Num extends ValueType[Number] {
+    override def defaultGen(t: AnyVal)(implicit mutate: AnyVal => Number): Number = mutate(t)
+  }
+
+  case object Date extends ValueType[LocalDateTime] {
+    override def defaultGen(t: AnyVal)(implicit mutate: AnyVal => LocalDateTime): LocalDateTime = mutate(t)
+  }
 
 }
 
