dc5837c7603c192d9a6fa2b6e6ed45636c8191b1
vood <balitsky_igor@list.ru>
2021-06-14 20:18:33 +0300

    a

--- src/main/scala/ru/vood/bigdata/generator/Main.scala
+++ src/main/scala/ru/vood/bigdata/generator/Main.scala
@@ -1,6 +1,5 @@
 package ru.vood.bigdata.generator
 
-import ru.vood.bigdata.generator.ent.Column
 import ru.vood.bigdata.generator.ent.clu.Clu
 import ru.vood.bigdata.generator.ent.intf.ValueType.{Date, Num, Str}
 import ru.vood.bigdata.generator.ent.intf.{EntityFun, MetaDelete}
@@ -13,37 +12,45 @@ object Main {
   implicit val totalMeta: Map[String, EntityFun] = MetaDelete.getMeta
 
   def main(args: Array[String]): Unit = {
-    val overrideScore = Map[String, String => String](("mer_sign", { q => q.hashCode.toString }), ("date_cr", { q => LocalDateTime.now().plusMinutes(q.hashCode).toString }))
-    val list = scoreFuns(totalMeta, "score", overrideScore).map { q => q._3 }
+    val overrideScore = Map[String, String => String](("mer_sign", { q => (Math.abs(q.hashCode) % 2).toString }), ("date_cr", { q => LocalDateTime.now().plusMinutes(q.hashCode).toString }))
+    val score = "score"
+    val list = scoreFuns(totalMeta, score, overrideScore).map { q => q._3 }
       .toList
 
-    val scoreData = (1 to 20)
+    val scoreData: Set[Score] = (1 to 2000000)
       .map { id =>
         Score(id.toString, /*overrideScore, */ list, { _ => Set[Clu]() })
       }
       .toSet
 
-    val value = scoreData.map { q => q.csvStr }.mkString("\n")
 
-    println(value)
+    import java.io.BufferedWriter
+    import java.io.FileWriter
+    val writer: BufferedWriter = new BufferedWriter(new FileWriter("e:/temp/"+score+".csv"))
+    scoreData.foreach(q => writer.write(q.csvStr+"\n") )
+
+    writer.close()
+
+//    val value = scoreData.map { q => q.csvStr }.mkString("\n")
+//
+//    println(value)
 
   }
 
   private def scoreFuns(meta: Map[String, EntityFun], nameEnt: String, overrideDefaults: Map[String, String => String]) = {
-    val scoreMeta = meta(nameEnt).cols
-    val value1 = scoreMeta.map { defFun =>
-
+    val value1 = meta(nameEnt).cols.map { defFun =>
       val function = defFun.valueType match {
         case Str => Str.stringConverter(Str.defaultStr)
         case Num => Num.stringConverter(Num.defaultNum)
         case Date => Date.stringConverter(Date.defaultDate)
         case _ => throw new IllegalStateException("asd")
       }
-
       val overrid = overrideDefaults.getOrElse(defFun.name, function)
       (defFun.name, defFun, overrid)
     }
     value1
   }
+
+
 }
 
--- src/main/scala/ru/vood/bigdata/generator/ent/intf/MetaDelete.scala
+++ src/main/scala/ru/vood/bigdata/generator/ent/intf/MetaDelete.scala
@@ -9,13 +9,20 @@ object MetaDelete {
 
   def getMeta = {
 
+    val value = Set(
+      Column("crm_id", Str, HZ),
+      Column("mer_sign", Num, AF),
+      Column("date_cr", Date, HZ),
+    )
+    val s = (1 to 30).map { q => Column("col_s_" + q, Str, HZ) }
+    val n = (1 to 30).map { q => Column("col_n_" + q, Num, AF) }
+    val d = (1 to 30).map { q => Column("col_d_" + q, Date, AF) }
+
+    val value1 = value ++ s ++ n ++ d
+
     val score = EntityFun(
       "score",
-      Set(
-        Column("crm_id", Str, HZ),
-        Column("mer_sign", Num, AF),
-        Column("date_cr", Date, HZ),
-      )
+      value1
     )
 
     val clu = EntityFun(
