a2e41bdf56ed2d33d2ded53f56210ec4b4ae1c01
vood <balitsky_igor@list.ru>
2021-06-18 23:43:44 +0300

    printer

--- src/main/scala/ru/vood/bigdata/generator/arbitrarry/Main.scala
+++ src/main/scala/ru/vood/bigdata/generator/arbitrarry/Main.scala
@@ -13,7 +13,7 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     CsvPrinter.print[ScoreHard]("score", 20, { id => GenScoreHard(id, weightedDictionary).genScore.sample },
-      { score => s"${score.id};${score.col_1};${score.col_2};${score.col_3};${score.col_4};${score.col_5};${score.col_6};${score.col_7};${score.col_8};${score.col_9};${score.col_10};${score.col_11};${score.col_12};${score.col_13};${score.col_14};${score.col_15};${score.col_16};${score.col_17};${score.col_18};${score.col_19};${score.col_20}" }
+      { score => s"${score.id};${score.col_1};${score.col_2};${score.col_3};${score.col_4}" }
     )
 
 
--- src/main/scala/ru/vood/bigdata/generator/arbitrarry/csv/CsvPrinter.scala
+++ src/main/scala/ru/vood/bigdata/generator/arbitrarry/csv/CsvPrinter.scala
@@ -6,7 +6,7 @@ import java.time.LocalDateTime
 object CsvPrinter {
 
   def print[T](ent: String, cnt: Int, gen: Int => Option[T], toStr: T => String) = {
-    val writerClu: BufferedWriter = new BufferedWriter(new FileWriter(s"e:/temp/${ent}Hard.csv"))
+    val fileWriter: BufferedWriter = new BufferedWriter(new FileWriter(s"e:/temp/${ent}Hard.csv"))
 
     val time: LocalDateTime = LocalDateTime.now()
     println(s"write $ent => " + time)
@@ -18,16 +18,16 @@ object CsvPrinter {
       })
       .map(opt => opt.get)
       .map(toStr)
-      .foreach(q => writerClu.write(q + "\n"))
+      .foreach(q => fileWriter.write(q + "\n"))
 
     val time1 = LocalDateTime.now()
     println(s"write end $ent => " + time1)
 
-    writerClu.close()
+    fileWriter.close()
 
-    val totalNano = (time1.getNano - time.getNano)
-    val perscore = totalNano.toDouble / cnt.toDouble
-    println(s"per $ent => $perscore totalNano => $totalNano cnt => $cnt")
+    val totalNano = time1.getNano - time.getNano
+    val persEnt = totalNano.toDouble / cnt.toDouble
+    println(s"per $ent => $persEnt totalNano => $totalNano cnt => $cnt")
 
   }
 
